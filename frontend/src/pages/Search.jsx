41c9268f2339942926464765914c304643a9e507
Joeyg61703 <Joeyg61703@gmail.com>
2022-09-21 15:13:15 -0400

    Add search Movies Functionality with /search endpoint

---
+++ frontend/src/pages/Search.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import SearchMovies from '../components/search/SearchMovies';
+
+const Search = () => {
+  return (
+    <>
+    <Header/>
+    <SearchMovies/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Search
